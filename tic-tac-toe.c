b77548bb418956e4fdb784a8b242d913f4f16179
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-02-23 00:14:38 -0600

    Version correcta

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,13 +1,20 @@
+/*
+    Intgrantes: {
+
+    }
+    fecha: 21/02/2025
+    tema: Matrices
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+main() {
     char gato[3][3];
 
     int i, j, band = 1, count = 0, turn = 1;
-
-    // Inicializar la matriz con caracteres individuales
+    int X = 'X', O = 'O';
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++) {
             gato[i][j] = '-';
@@ -15,8 +22,8 @@ int main() {
     }
 
     while(band == 1 && count < 9) {
-        // Imprimir la matriz
-        printf("\n");
+        int posicion, cordY, cordX;
+
         for(i = 0; i < 3; i++) {
             for(j = 0; j < 3; j++) {
                 if(j == 0)
@@ -28,73 +35,109 @@ int main() {
             }
         }
 
-        // Mostrar turno actual
         if(turn == 1) {
-            printf("\nTurno del Jugador X\n");
-            printf("Introduce una posicion del 1 al 9: ");
+            printf("\nTurno 1\n");
+            printf("Introduce una posiscion del 1 al 9: ");
         } else {
-            printf("\nTurno del Jugador O\n");
-            printf("Introduce una posicion del 1 al 9: ");
+            printf("Turno 2\n");
+            printf("Introduce una posiscion del 1 al 9: ");
         }
 
-        int posicion;
         scanf("%d", &posicion);
 
-        int cordY = (posicion - 1) / 3;
-        int cordX = (posicion - 1) % 3;
+        cordY = (posicion - 1) / 3;
+        cordX = (posicion - 1) % 3;
 
-        // Actualizar la posicion correctamente
         if(gato[cordY][cordX] == '-' && turn == 1)
             gato[cordY][cordX] = 'X';
         else if(gato[cordY][cordX] == '-' && turn == -1)
             gato[cordY][cordX] = 'O';
+        else {
+            printf("\nEsa posicion ya esta definida, intenta denuevo\n");
+            count--;
+        }
 
-        // Verificar ganador horizontalmente
         for(i = 0; i < 3; i++) {
-            if(gato[i][0] == 'X' && gato[i][1] == 'X' && gato[i][2] == 'X') {
-                printf("\nEl jugador X es el ganador!\n");
+            if(gato[i][0] == X && gato[i][1] == X && gato[i][2] == X) {
+                printf("\nEl jugador 1 es el ganador");
                 band = 0;
+                printf("\nfirst point\n");
                 break;
             }
-            if(gato[i][0] == 'O' && gato[i][1] == 'O' && gato[i][2] == 'O') {
-                printf("\nEl jugador O es el ganador!\n");
+
+            if(gato[i][0] == O && gato[i][1] == O && gato[i][2] == O) {
+                printf("\nEl jugador 2 es el ganador");
                 band = 0;
+                printf("\nfirst point\n");
                 break;
             }
+
         }
 
-        // Verificar ganador verticalmente
         for(i = 0; i < 3; i++) {
-            if(gato[0][i] == 'X' && gato[1][i] == 'X' && gato[2][i] == 'X') {
-                printf("\nEl jugador X es el ganador!\n");
+            if(gato[0][i] == X && gato[1][i] == X && gato[2][i] == X) {
+                printf("\nEl jugador 1 es el ganador");
                 band = 0;
+                printf("\nsecond point\n");
                 break;
             }
-            if(gato[0][i] == 'O' && gato[1][i] == 'O' && gato[2][i] == 'O') {
-                printf("\nEl jugador O es el ganador!\n");
+
+            if(gato[0][i] == O && gato[1][i] == O && gato[2][i] == O) {
+                printf("\nEl jugador 2 es el ganador");
                 band = 0;
+                printf("\nsecond point\n");
                 break;
             }
         }
 
-        // Verificar diagonales
-        if((gato[0][0] == 'X' && gato[1][1] == 'X' && gato[2][2] == 'X') ||
-           (gato[0][2] == 'X' && gato[1][1] == 'X' && gato[2][0] == 'X')) {
-            printf("\nEl jugador X es el ganador!\n");
+        if (gato[0][0] == X && gato[1][1] == X && gato[2][2] == X) {
+            printf("\nEl jugador 1 es el ganador");
             band = 0;
+            printf("\nthird point\n");
+            break;
         }
-        else if((gato[0][0] == 'O' && gato[1][1] == 'O' && gato[2][2] == 'O') ||
-                (gato[0][2] == 'O' && gato[1][1] == 'O' && gato[2][0] == 'O')) {
-            printf("\nEl jugador O es el ganador!\n");
+
+        if (gato[0][0] == O && gato[1][1] == O && gato[2][2] == O) {
+            printf("\nEl jugador 2 es el ganador");
             band = 0;
+            printf("\nthird point\n");
+            break;
+        }
+
+        if (gato[0][2] == X && gato[1][1] == X && gato[2][0] == X) {
+            printf("\nEl jugador 1 es el ganador");
+            band = 0;
+            printf("\nfourth point\n");
+            break;
+        }
+
+        if (gato[0][2] == O && gato[1][1] == O && gato[2][0] == O) {
+            printf("\nEl jugador 2 es el ganador");
+            band = 0;
+            printf("\nfourth point\n");
+            break;
         }
 
         if(band == 0)
             break;
 
         count++;
-        turn *= -1;
+        turn = turn * -1;
     }
 
+    printf("\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            if(j == 0)
+                printf("[ %c ", gato[i][j]);
+            else if(j == 1)
+                printf("| %c ", gato[i][j]);
+            else
+                printf("| %c ]\n", gato[i][j]);
+        }
+    }
+
+    getch();
     return 0;
 }
